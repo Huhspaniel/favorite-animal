a6554988e8f489d0a4c4e18770af87ffaba91ff9
Huhspaniel <41580234+Huhspaniel@users.noreply.github.com>
2018-09-26 13:07:42 -0400

    Nested router-outlet in div.content, removed from home.component.html Generated AccountComponent, FriendsComponent, PostsComponent Added routing to sidebar for account, friends, and posts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,26 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { AccountComponent } from './account/account.component';
+import { FriendsComponent } from './friends/friends.component';
+import { PostsComponent } from './posts/posts.component';
 
 const routes: Routes = [
 	{
 		path: '',
 		component: HomeComponent
+	},
+	{
+		path: 'account',
+		component: AccountComponent
+	},
+	{
+		path: 'friends',
+		component: FriendsComponent
+	},
+	{
+		path: 'posts',
+		component: PostsComponent
 	}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,19 @@ import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
+import { AccountComponent } from './account/account.component';
+import { FriendsComponent } from './friends/friends.component';
+import { PostsComponent } from './posts/posts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    AccountComponent,
+    FriendsComponent,
+    PostsComponent
   ],
   imports: [
     BrowserModule,
