6fc0863e7772f9e112ab7febc93760cb46434855
Huhspaniel <41580234+Huhspaniel@users.noreply.github.com>
2018-09-28 00:49:18 -0400

    Added heading to friends page Generate list of placeholder friends

--- src/app/friends/friends.component.ts
+++ src/app/friends/friends.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-friends',
-  templateUrl: './friends.component.html',
-  styleUrls: ['./friends.component.scss']
+	selector: 'app-friends',
+	templateUrl: './friends.component.html',
+	styleUrls: ['./friends.component.scss']
 })
 export class FriendsComponent implements OnInit {
 
-  constructor() { }
+	friends: object[] = [];
 
-  ngOnInit() {
-  }
+	constructor() {
+		for (let i = 0; i < 10; i++) {
+			this.friends.push(
+				{
+					name: `friend${i}`,
+					username: `user${i}`,
+				}
+			);
+		}
+	}
+
+	ngOnInit() {
+	}
 
 }
